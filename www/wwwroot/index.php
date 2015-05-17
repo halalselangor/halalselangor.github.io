c47d4d8c85812448526f4dc00c6dfcc0412f37a1
silentrader <silentraderv2.0@gmail.com>
2015-05-18 01:56:31 +0800

    First commit
    Just do it

---
+++ www/wwwroot/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Local</title>
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body class="body-1">
+  <div class="w-section head">
+    <div class="w-container"><a class="kembali" href="#">KEMBALI KE PORTAL HALAL SELANGOR</a>
+    </div>
+  </div>
+  <div class="w-section wrapper">
+    <div class="w-container header"><img class="selangor" src="images/Lambang Selangor.png" width="80"><img class="jais" src="images/jais.png" width="97">
+      <h1>SISTEM PENTADBIRAN DAN PENGURUSAN<br>PENYEMBELIHAN NEGERI SELANGOR</h1>
+    </div>
+    <div class="w-container main-container">
+      <div class="w-row">
+        <div class="w-col w-col-6 login">
+          <h2>Log Masuk Pengguna</h2>
+          <div class="w-form">
+            <form class="login-form" id="wf-form-login-form" name="wf-form-login-form" data-name="login form">
+              <label class="username" for="username">ID PENGGUNA:</label>
+              <input class="w-input user" id="username" type="text" placeholder="Masukkan ID Pengguna" name="username" data-name="username" required="required">
+              <label class="password" for="password">KATA LALUAN:</label>
+              <input class="w-input" id="password" type="password" placeholder="Masukkan Kata Laluan anda" name="password" data-name="password" required="required">
+              <a class="w-inline-block button" href="/admin/home">
+                <div>HANTAR</div>
+              </a>
+            </form>
+            <div class="w-form-done success">
+              <p>Thank y!</p>
+            </div>
+            <div class="w-form-fail">
+              <p>Oops! Something went wrong while submitting the form</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-col w-col-6 announcement">
+          <h2>Pengumuman</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-section footer">
+    <div class="copyright">Hakcipta terpelihara Jabatan Agama Islam Selangor @ 2015</div>
+  </div>
+    </body>
+</html>
